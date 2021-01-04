aedabd14b131b093cf72622fe281a7713d892990
Joao Vicente <joao@upsites.digital>
2021-01-04 00:04:31 -0300

    Aula sobre estruturas condicionais aninhadas, primeiro commit valendo

--- aulas/aula12a.py
+++ aulas/aula12a.py
@@ -1,1 +1,8 @@
-print('Teste')
+nome = str(input('Qual o seu o nome? '))
+if nome == 'João':
+	print('Que nome bonito!')
+elif nome == 'José' or  nome == 'Maria' or nome == 'Paulo':
+	print('Seu nome é bem Popular no Brasil!')	
+else:
+	print('Seu nome é  bem normal!')
+print('Tenha um bom dia {}'.format(nome))
