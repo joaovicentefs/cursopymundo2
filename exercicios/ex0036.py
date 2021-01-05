b627da73718709104eac11ebe29594c62a1ecec6
Joao Vicente <joao@upsites.digital>
2021-01-05 18:08:22 -0300

    Exercício finalizado

--- exercicios/ex0036.py
+++ exercicios/ex0036.py
@@ -2,7 +2,9 @@ vCasa = float(input('Valor da Casa: R$ '))
 salario = float(input('Salário do comprador: R$ '))
 tempo = int(input('Quantos anos de financiamento? '))
 prestacao = vCasa / (tempo * 12)
-print('Para pagar uma casa de R$ {:.2f} em {} anos a prestação será de R$ {:.2f}'.format(vCasa, tempo, prestacao))
+# Quebrei o print em dois apenas para mostrar o uso do Recurso end= que deixa dois prints em uma linha só
+print('Para pagar uma casa de R$ {:.2f} em {} anos'.format(vCasa, tempo), end='')
+print(' a prestação será de R$ {:.2f}'.format(prestacao))
 if prestacao <= salario * (30 /100):
 	print('Empréstimo APROVADO!')
 else:
