dadee10fe762c2561139cb6f868f8b2886f82b56
Joao Vicente <joao@upsites.digital>
2021-01-04 23:35:15 -0300

    Terminando a aula 12

--- aulas/aula12a.py
+++ aulas/aula12a.py
@@ -1,8 +1,11 @@
+# Aprendendo condições aninhadas
 nome = str(input('Qual o seu o nome? '))
 if nome == 'João':
 	print('Que nome bonito!')
 elif nome == 'José' or  nome == 'Maria' or nome == 'Paulo':
-	print('Seu nome é bem Popular no Brasil!')	
+	print('Seu nome é bem Popular no Brasil!')
+elif nome in 'Ana Maria Paula Gabriela':
+	print('Belo nome feminino')
 else:
 	print('Seu nome é  bem normal!')
 print('Tenha um bom dia {}'.format(nome))
