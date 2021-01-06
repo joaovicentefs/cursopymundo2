cabb74cf142da4a1afe3daf28ed0e2f4e61a8dd4
Joao Vicente <joao@upsites.digital>
2021-01-05 23:58:25 -0300

    Versão final do exercício 39

--- exercicios/ex0039.py
+++ exercicios/ex0039.py
@@ -2,14 +2,12 @@ from datetime import date
 anoNascimento = int(input('Ano de nascimento: '))
 anoAtual = date.today().year
 idade = anoAtual - anoNascimento
+print('Quem nasceu em {} tem {} anos em {}.'.format(anoNascimento, idade, anoAtual))
 if idade < 18:
-	print('''Quem nasceu em {} tem {} anos em {}.
-	Ainda faltam {} ano(s) para o alistamento.
-	Seu alistamento será em {}'''.format(anoNascimento, idade, anoAtual, 18 - idade, anoAtual + (18 - idade)))
+	print('''Ainda faltam {} ano(s) para o alistamento.
+	Seu alistamento será em {}'''.format(18 - idade, anoAtual + (18 - idade)))
 elif idade > 18:
-	print('''Quem nasceu em {} tem {} anos em {}.
-	Você já deveria ter se alistado há {} ano(s).
-	Seu alistamento foi em {}.'''.format(anoNascimento, idade, anoAtual, abs(18 - idade), anoAtual - (idade - 18)))
-else:
-	print('''Quem nasceu em {} tem {} anos em {}.
-	Você tem que se alistar IMEDIATAMENTE!'''.format(anoNascimento, idade, anoAtual))
+	print('''Você já deveria ter se alistado há {} ano(s).
+	Seu alistamento foi em {}.'''.format(abs(18 - idade), anoAtual - (idade - 18)))
+elif idade < 18:
+	print('Você tem que se alistar IMEDIATAMENTE!')
