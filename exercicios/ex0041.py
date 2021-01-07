204743774cf5b3243ad593ac20436e3178fb88ed
Joao Vicente <joao@upsites.digital>
2021-01-07 14:09:18 -0300

    Versão final do exercício 41 com simplificações do Guanabara

--- exercicios/ex0041.py
+++ exercicios/ex0041.py
@@ -4,13 +4,15 @@ nasc = int(input('Informe seu ano de nascimento: '))
 idade = hoje - nasc
 txt_padrao = 'Sua Categoria:'
 print('O atleta tem {} anos.'.format(idade))
+'''Não preciso usar o 9 < no primeiro elif porque se ele chegou nesta condição é que ele passou 
+pela outra, com isso podemos simplificar todos os elif'''
 if idade <= 9:
 	print(txt_padrao, 'MIRIM')
-elif 9 < idade <= 14:
+elif idade <= 14:
 	print(txt_padrao, 'INFANTIL')
-elif 14 < idade <= 19:
+elif idade <= 19:
 	print(txt_padrao, 'JÚNIOR')
-elif 19 < idade <= 25:
+elif idade <= 25:
 	print(txt_padrao, 'SÊNIOR')
 elif idade > 25:
 	print(txt_padrao, 'MASTER')
