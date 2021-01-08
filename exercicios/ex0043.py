14439b8cb40e65b5458d3faebf28310a7a757cf1
Joao Vicente <joao@upsites.digital>
2021-01-08 13:47:41 -0300

    Versão final exercício 43 com ajuste do Guanabara

--- exercicios/ex0043.py
+++ exercicios/ex0043.py
@@ -4,11 +4,11 @@ imc = float(peso / pow(altura, 2))
 print('O IMC desta pessoa é de {:.1f}'.format(imc))
 if imc < 18.5:
 	print('Cuidado você está ABAIXO DO PESO NORMAL')
-elif 18.5 >= imc < 25:
+elif 18.5 <= imc < 25:
 	print('PARABÉNS, você está na faixa de PESO NORMAL')
-elif 25 >= imc < 30:
+elif 25 <= imc < 30:
 	print('Atenção! Você está com SOBREPESO! Alimente-se melhor.')
-elif 30 >= imc < 40:
+elif 30 <= imc < 40:
 	print('Alerta vermelho! Você já está na fase de OBSEDIDADE')
-elif imc > 40:
+elif imc >= 40:
 	print('Seu caso é grave. Você está na faixa de OBESIDADE MÓRBIDA, procure um médico!')
