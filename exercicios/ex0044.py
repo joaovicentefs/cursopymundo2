a2404e34ecbc4cd553c7d1396f357eab520119e0
Joao Vicente <joao@upsites.digital>
2021-01-09 13:14:17 -0300

    Versão final com melhorias do Guanabara

--- exercicios/ex0044.py
+++ exercicios/ex0044.py
@@ -1,4 +1,5 @@
-total = float(input('Informe o preço das compras: R$ '))
+print('{:=^40}'.format(' LOJAS UPSITES '))
+compras = float(input('Informe o preço das compras: R$ '))
 print('''FORMAS DE PAGAMENTO:
 [ 1 ] à vista dinheiro/cheque
 [ 2 ] à vista no cartão
@@ -6,13 +7,19 @@ print('''FORMAS DE PAGAMENTO:
 [ 4 ] 3x ou mais no cartão''')
 opcao = int(input('Qual é a opção? '))
 if opcao == 1:
-	print('Sua compra de R$ {:.2f} vai custar R$ {:.2f} no final.'.format(total, total - (total * 0.10)))
+	total = compras - (compras * 0.10) 
 elif opcao == 2:
-	print('Sua compra de R$ {:.2f} vai custar R$ {:.2f} no final.'.format(total, total - (total * 0.05)))
+	total = compras - (compras * 0.05)
 elif opcao == 3:
-	print('Sua compra de R$ {:.2f} vai custar R$ {:.2f} no final.'.format(total, total))
+	total = compras
+	parcela = total / 2
+	print('Sua compra será parcela em 2x de R$ {:.2f} SEM JUROS.'.format(parcela))
 elif opcao == 4:
+	total = compras + (compras * 0.20)
 	nparcelas = int(input('Quantas parcelas? '))
-	vparcelas = float((total + (total * 0.20)) / nparcelas)
+	vparcelas = float((compras + (compras * 0.20)) / nparcelas)
 	print('Sua compra será parcelada em {}x de R$ {:.2f} COM JUROS'.format(nparcelas, vparcelas))
-	print('Sua compra de R$ {:.2f} vai custar R$ {:.2f} no final.'.format(total, total + (total * 0.20)))
+else:
+	total = compras
+	print('OPÇÃO INVÁLIDA de pagamento. Tente novamente!')
+print('Sua compra de R$ {:.2f} vai custar R$ {:.2f} no final.'.format(compras, total))
