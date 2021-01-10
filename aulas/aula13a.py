be5e2404c43ab2e5f98e89b7748a43f44b63ac0c
Joao Vicente <joao@upsites.digital>
2021-01-10 11:21:10 -0300

    Primeiro exemplo usando for no Python

---
+++ aulas/aula13a.py
@@ -0,0 +1,5 @@
+# For também é conhecida como laço
+# No exemplo abaixo o 'c' poderia ser substituido por qualquer valor, o 'c' é apenas uma convenção.
+for c in range(0, 6): #O ultimo valor ele ignora o último valor
+	print('Oi')
+print('Fim')
