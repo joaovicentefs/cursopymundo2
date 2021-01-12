8a17c0971fbd79166a63ca3615e071b303bf849a
Joao Vicente <joao@upsites.digital>
2021-01-11 23:29:14 -0300

    Versão final com ajustes do Guanabara

--- exercicios/ex0048.py
+++ exercicios/ex0048.py
@@ -1,5 +1,7 @@
 soma = 0
-for c in range(3, 501, 3):
-	if c % 2 != 0:
+cont = 0
+for c in range(1, 501, 2):
+	if c % 3 == 0:
 		soma = soma + c
-print('O resultado final de todos os números é de {}'.format(soma))
+		cont = cont +1
+print('O resultado final de todos os {} números é de {}'.format(cont, soma))
