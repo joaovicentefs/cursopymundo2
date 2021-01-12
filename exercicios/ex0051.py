295f7184f22b525933a75efb71f4fecc783ed812
Joao Vicente <joao@upsites.digital>
2021-01-12 16:56:03 -0300

    Versão final do exercício 51 COM ajuda do Guanabara

--- exercicios/ex0051.py
+++ exercicios/ex0051.py
@@ -1,13 +1,20 @@
-cont = 0
+# cont = 0
 print('='*30)
 txt = '10 TERMOS DE UMA PA'
 x = txt.center(30,' ')
 print(x)
 print('='*30)
-num = int(input('Primeiro termo: '))
+'''num = int(input('Primeiro termo: '))
 raz = int(input('Razão: '))
 for c in range(num, 9999, raz):
 	cont += 1
 	if cont <= 10:
 		print(c, '--> ', end='')
+print('ACABOU')'''
+# Versão do Guanabara
+primeiro = int(input('Primeiro termo: '))
+raz = int(input('Razão: '))
+decimo = primeiro + (10 -1) * raz
+for c in range(primeiro, decimo + raz, raz):
+	print(c, end=' --> ')
 print('ACABOU')
