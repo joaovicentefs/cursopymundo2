49295a9279f7dfab206b5947ca296896419a360f
Joao Vicente <joao@upsites.digital>
2021-01-12 23:10:38 -0300

    Versão final do exercício 52 com ajustes do Guanabara

--- exercicios/ex0052.py
+++ exercicios/ex0052.py
@@ -1,10 +1,13 @@
 cont = 0
 num = int(input('Digite um número: '))
 for c in range(1, num + 1):
-	divi = num % c
-	if divi == 0:
+	if num % c == 0:
+		print('\033[33m', end='')
 		cont += 1
-print('O número {} foi divísivel {} vezes'.format(num, cont))
+	else:
+		print('\033[31m', end='')
+	print('{} '.format(c), end='')
+print('\n\033[mO número {} foi divísivel {} vezes'.format(num, cont))
 if cont == 2:
 	print('E por isso ele É PRIMO')
 else:
