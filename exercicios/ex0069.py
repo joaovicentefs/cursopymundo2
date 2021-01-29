b7585fb01d4712ca3c8dda418f1ec57d02a7629c
Joao Vicente <joao@upsites.digital>
2021-01-29 20:38:43 -0300

    Versão final do Exercício 69 com melhorias do Guanabara

--- exercicios/ex0069.py
+++ exercicios/ex0069.py
@@ -1,19 +1,21 @@
 totI = totM = totF20 = 0
-continua = ''
 while True:
 	print('-' * 40)
 	print('{:^40}'.format('CADASTRE UMA PESSOA'))
 	print('-' * 40)
 	idade = int(input('Idade: '))
-	sexo = str(input('Sexo: [M/F] ')).strip().upper()[0]
+	sexo = ' '# Importante que aqui seja uma string com espaço em branco e não vazia
+	while sexo not in 'MF':
+		sexo = str(input('Sexo: [M/F] ')).strip().upper()[0]
 	print('-' * 40)
-	continua = str(input('Quer continuar?  [S/N] ')).strip().upper()[0]
+	continua = ' '
+	while continua not in 'SN':
+		continua = str(input('Quer continuar?  [S/N] ')).strip().upper()[0]
 	if idade > 18:
 		totI += 1
 	if sexo == 'M':
 		totM += 1
-	if sexo == 'F':
-		if idade < 20:
+	if sexo == 'F' and idade < 20:
 			totF20 += 1
 	if continua == 'N':
 		break
