ac3f7bd2dfde1d87c9278efcba24073d0bf3fef9
Joao Vicente <joao@upsites.digital>
2021-02-01 18:03:40 -0300

    Versão final do exercício 70 com correções do Guanabara

--- exercicios/ex0070.py
+++ exercicios/ex0070.py
@@ -1,19 +1,23 @@
-gasto = mais_caro = 0
-item_barato = ' '
+gasto = mais_caro = cont = mais_barato = 0
+item_barato = ''
 print('-' * 40)
 print('{:^40}'.format('LOJÃO DO JOÃO'))
 print('-' * 40)
 while True:
 	produto = str(input('Nome do produto: ')).strip().lower()
 	preco = float(input('Preço: R$ '))
-	mais_barato = preco
+	cont += 1
 	gasto += preco
 	resp = ' '
 	if preco > 1000:
 		mais_caro += 1
-	if preco < mais_barato:
+	if cont == 1:
 		mais_barato = preco
 		item_barato = produto
+	else:
+		if preco < mais_barato:
+			mais_barato = preco
+			item_barato = produto
 	while resp not in 'SN':
 		resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
 	if resp == 'N':
