e0b475581979d52bdaf54513fdd579ee3756df1b
Joao Vicente <joao@upsites.digital>
2021-02-02 12:56:49 -0300

    Versão final com melhorias do Guanabara

--- exercicios/ex0072.py
+++ exercicios/ex0072.py
@@ -1,5 +1,7 @@
 extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezesete', 'dezoito', 'dezenove', 'vinte')
-numero = 21
-while numero not in range(0, 21):
+while True:
 	numero = int(input('Digite um número entre 0 e 20: '))
+	if 0 <= numero <= 20:
+		break
+	print('Tente novamente. ', end='')
 print(f'Você digitou o número {extenso[numero]}')
