26f42b1406295438a58c499c3951ce7d00ff8780
Joao Vicente <joao@upsites.digital>
2021-02-02 14:34:38 -0300

    Primeira versão do exercício 73 sem a ajuda do Guanabara

---
+++ exercicios/ex0073.py
@@ -0,0 +1,14 @@
+times = ('PALMEIRAS', 'FLAMENGO', 'INTERNACIONAL', 'GRÊMIO',
+		'SÃO PAULO', 'ATLÉTICO-MG', 'ATHLETICO', 'CRUZEIRO', 'BOTAFOGO',
+		'SANTOS', 'BAHIA', 'FLUMINENSE', 'CORINTHIANS', 'CHAPECOENSE',
+		'CEARÁ', 'VASCO', 'SPORT', 'AMÉRICA-MG', 'VITÓRIA', 'PARANÁ')
+print('-=' * 15)
+print(f'Lista de times do brasileirão: {times}')
+print('-=' * 15)
+print(f'Os 5 primeiros times são: {times[:5]}')
+print('-=' * 15)
+print(f'Os Quatro últimos são: {times[-5:]}')
+print('-=' * 15)
+print(f'Os timnes em ordem alfabética: {sorted(times)}')
+print('-=' * 15)
+print('O Chapecoense está na {}ª Posição'.format(times.index('CHAPECOENSE') + 1))
