0713c4997c2076761fe2a9b96f1a8f4a9b8d51ad
Joao Vicente <joao@upsites.digital>
2021-02-02 23:27:42 -0300

    Versão com otimização de código com a ajuda do Guanabara

--- exercicios/ex0074.py
+++ exercicios/ex0074.py
@@ -2,14 +2,11 @@ from random import randint
 maior = menor = cont = 0
 sorteio = (randint(0,10), randint(0,10), randint(0,10), randint(0,10), randint(0,10))
 print('Os valores sorteados foram: ', end='')
-maior = menor = 0
+# maior = menor = 0
 for num in sorteio:
 	cont += 1
-	if cont != 5:
-		print(f'{num}', end=' ')
-	else:
-		print(f'{num}')	
-	if cont == 1:
+	print(f'{num}', end=' ')
+	'''if cont == 1:
 		maior = menor = num
 	else:
 		if num == 0:
@@ -19,4 +16,6 @@ for num in sorteio:
 		if num > maior:
 			maior = num
 print(f'O maior valor sorteado foi {maior}')
-print(f'O menor valor sorteado foi {menor}')
+print(f'O menor valor sorteado foi {menor}')'''
+print(f'\nO maior valor sorteado foi {max(sorteio)}') # para tuplas podemos usar a função max e min
+print(f'O menor valor sorteado foi {min(sorteio)}')
