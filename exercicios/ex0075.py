e996cc2201c4c4b0c89f4286636c982faf5bde83
Joao Vicente <joao@upsites.digital>
2021-02-03 22:35:22 -0300

    Versão final do exercício 75 com ajustes do Guanabara

--- exercicios/ex0075.py
+++ exercicios/ex0075.py
@@ -3,10 +3,15 @@ resp = (int(input('Digite um número: ')),
 		int(input('Digite mais um número: ')), 
 		int(input('Digite o último número: '))
 		)
-print(f'O valor 9 apareceu {resp.count(9)} vezes')
-print(f'O valor 3 apareceu a primeira vez na {(resp.index(3) + 1)}ª posição')
+print('Os números digitados foram:', end='')
+for n in resp:
+	print(f' {n}', end='')
+print(f'\nO valor 9 apareceu {resp.count(9)} vezes')
+if 3 in resp:
+	print(f'O valor 3 apareceu a primeira vez na {(resp.index(3) + 1)}ª posição')
+else:
+	print('O valor 3 não foi digitado em nenhuma posição')
 print('Os valores pares digitados foram: ', end='')
 for num in resp:
-
 	if num % 2 == 0:
 		print(num, end=' ')
