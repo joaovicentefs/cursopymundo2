bde0c1e9b779f5fc1411e9a74126fb11016ceb0c
Joao Vicente <joao@upsites.digital>
2021-02-08 16:35:27 -0300

    Versão final do exercício 81 com otimização e redução do código com ajuda do guanabara, o principal foi o uso do in para buscar o valor dentro da lista

--- exercicios/ex0081.py
+++ exercicios/ex0081.py
@@ -1,21 +1,14 @@
 lista = list()
-cont = 0
 while True:
 	lista.append(int(input('Digite um valor: ')))
-	cont += 1
 	resp = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
 	if resp == 'N':
 		break
 print('-=' * 30)
-print(f' Você digitou {cont} elementos.')
+print(f'Você digitou {len(lista)} elementos.')
 lista.sort(reverse=True)
 print(f'Os valores em ordem decrescente são {lista}')
-for n in lista:
-	if n == 5:
-		cinco = True
-	else:
-		cinco = False
-if cinco == True:		
+if 5 in lista:		
 	print('O valor 5 faz parte da lista.')
 else:
 	print('O valor 5 não foi encontrado na lista')
