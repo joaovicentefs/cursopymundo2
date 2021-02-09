0c01cccafcaa3251040c94f057071a49cebe9648
Joao Vicente <joao@upsites.digital>
2021-02-08 22:53:42 -0300

    Versão muito longa do exercício 83, sugestão do Guanabara

--- exercicios/ex0083.py
+++ exercicios/ex0083.py
@@ -1,6 +1,22 @@
 expressao = str(input('Didigite a expressão: ')).strip()
-lista = list(expressao)
+# minha versão
+'''lista = list(expressao)
 if lista.count('(') == lista.count(')'):
+	print('Sua expressão está válida')
+else:
+	print('Sua expressão está errada!')'''
+# Versão do Guanabara
+lista = list()
+for simb in expressao:
+	if simb == '(':
+		lista.append('(')
+	elif simb == ')':
+		if len(lista) > 0:
+			lista.pop()
+		else:
+			lista.append(')')
+			break
+if len(lista) == 0:
 	print('Sua expressão está válida')
 else:
 	print('Sua expressão está errada!')
