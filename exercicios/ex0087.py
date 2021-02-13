5e67ebf8d422295884ef81be1ef0cdddd2c09fb9
Joao Vicente <joao@upsites.digital>
2021-02-13 14:00:15 -0300

    Minha versão do exercício 87 com um erro no pedido c do enunciado, sem ajuda do guanabara

---
+++ exercicios/ex0087.py
@@ -0,0 +1,23 @@
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+somap = soma3 =  maiorl2 = 0
+for l in range(0, 3):
+	for c in range(0, 3):
+		matriz[l][c] = int(input(f'Digite um valor para [{l + 1}, {c + 1}]: '))
+		if matriz[l][c] % 2 == 0:
+			somap += matriz[l][c]
+		if c == 2:
+			soma3 += matriz[l][c]
+		if len(matriz) == 1 and c == 0:
+			maiorl2 = matriz[l][c]
+		elif len(matriz) == 1 and matriz[l][c] > maiorl2:
+			maiorl2 = matriz[l][c]
+print('-=' *30)
+for l in range(0, 3):
+	for c in range(0, 3):
+		print(f'[{matriz[l][c]:^5}]', end='')
+	print()
+print('-=' * 30)
+print(f'A soma dos valores pares é {somap}.')
+print(f'A soma dos valores da terceira coluna é {soma3}.')
+print(f'O maior valor da segunda linha é {maiorl2}.')
+
