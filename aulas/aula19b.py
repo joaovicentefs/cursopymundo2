18ce3b2353abc52e0d8fa323a2826b54840b7b31
Joao Vicente <joao@upsites.digital>
2021-02-15 10:53:00 -0300

    Mais exemplos da aula 19 sobre Dicionários em Python

---
+++ aulas/aula19b.py
@@ -0,0 +1,9 @@
+brasil = list()
+estado1 = {'nome': 'Bahia', 'uf': 'BA'}
+estado2 = {'nome': 'São Paulo', 'uf': 'SP'}
+brasil.append(estado1)
+brasil.append(estado2)
+print(estado1)
+print(estado2)
+print(brasil)
+print(brasil[0]['nome'])
