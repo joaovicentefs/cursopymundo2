dbc1e8200f2aa0c2a5c2ba54ad5211296e355ec7
Joao Vicente <joao@upsites.digital>
2021-02-14 22:10:13 -0300

    Versão final do exerício 88 com ajuda do Guanabara ára não aceitar numeros duplicados no jogo de cada palpite

--- exercicios/ex0088.py
+++ exercicios/ex0088.py
@@ -6,17 +6,23 @@ print('-' * 40)
 print('{:^40}'.format("JOGA NA MEGA SENA"))
 print('-' * 40)
 qtd = int(input('Quantos jogos você quer que eu sorteie? '))
-for c in range(1, qtd + 1):
-	for c in range(0, 6):
-		jogo.append(randint(1, 60))
+tot = 1
+while tot <= qtd:
+	cont = 0
+	while True:
+		num = randint(1, 60)
+		if num not in jogo:
+			jogo.append(num)
+			cont += 1
+		if cont >= 6:
+			break
+	jogo.sort()	
 	palpites.append(jogo[:])
 	jogo.clear()
+	tot += 1
 print('-=' * 3,f'SORTEANDO {qtd} JOGOS', '-=' * 3)
 for j in range(1, qtd +1):
 	palpites[j -1].sort()
 	print(f'Jogo {j}: {palpites[j - 1]}')
-	sleep(0.5)
+	sleep(1)
 print('-=' * 5,' < BOA SORTE > ', '-=' * 5)
-# MEU PROGRAMA FUNCIONA
-# Preciso fazer a melhoria nele para que ele não permita repetição de número dentro da lista
-# Ver a solução do Guanarabara para isso
