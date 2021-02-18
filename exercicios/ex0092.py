1c612841c0bda42ed90eed5b64aeefe8b70f01d0
Joao Vicente <joao@upsites.digital>
2021-02-17 22:14:54 -0300

    Versão final do exercício 92 com ajuda do Guanabara

--- exercicios/ex0092.py
+++ exercicios/ex0092.py
@@ -1,11 +1,16 @@
+from datetime import datetime
 trabalhador = dict()
 trabalhador['nome'] = str(input('Nome: '))
-trabalhador['nasc'] = int(input('Ano de Nascimento: '))
+nasc = int(input('Ano de Nascimento: '))
+trabalhador['idade'] = datetime.now().year - nasc
 ctps = int(input('Carteira de Trabalho (0 não tem): '))
 if ctps != 0:
 	trabalhador['ctps'] = ctps
+	trabalhador['contratacao'] = int(input('Ano de contratação: '))
+	trabalhador['salario'] = float(input('Salário: R$ '))
+	trabalhador['aposentadoria'] = trabalhador['idade'] + ((trabalhador['contratacao'] + 35) - datetime.now().year)
 else:
 	trabalhador['ctps'] = 'não cadastrado'
-print(trabalhador)
-#Antes de incluir os dados colocar como variável e depois inserir no dicionário
-#Importa o datetime
+print('-=' * 30)
+for k, v in trabalhador.items():
+	print(f'- {k} tem o valor {v}')
