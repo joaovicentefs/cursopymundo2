9b997de6c49fbb44c8b870acf6e6b01d623d4a48
Joao Vicente <joao@upsites.digital>
2021-02-21 23:32:34 -0300

    versão final do exercício 100 Com dica de melhoria do Guanabara

--- exercicios/ex0100.py
+++ exercicios/ex0100.py
@@ -3,12 +3,13 @@ from time import sleep
 numeros = []
 
 
-def sorteia():
+def sorteia(lst):
     print('Sorteando 5 valores da lista: ', end='')
     for n in range(0, 5):
         num = randint(0, 10)
-        numeros.append(num)
+        lst.append(num)
         print(num, end=' ')
+        sleep(0.3)
     print('PRONTO!')
 
 
@@ -20,5 +21,5 @@ def somaPar(lst):
     print(f'Somando os valores pares de {lst}, temos {somap}')
 
 
-sorteia()
+sorteia(numeros)
 somaPar(numeros)
