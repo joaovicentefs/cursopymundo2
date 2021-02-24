8a1dbf6118bff408af9c7204d04b886e5c5cb796
Joao Vicente <joao@upsites.digital>
2021-02-23 22:32:13 -0300

    Versão final do exercíco 104, preciso entender melhor sobre uso de return nas funções

---
+++ exercicios/ex0104.py
@@ -0,0 +1,17 @@
+def leiaInt(msg):
+    ok = False
+    valor = 0
+    while True:
+        num = str(input(msg))
+        if num.isnumeric():
+            valor = int(num)
+            ok = True
+        else:
+            print('\033[0;31mERRO! Digite um número inteiro válido.\033[m')
+        if ok:
+            break
+    return valor
+
+
+n = leiaInt('Digite um número: ')
+print(f'Você acabou de digitar o número {n}')
