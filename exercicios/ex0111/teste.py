f2de56d54de603dad4a803c8b5a3e00b2dd44e90
Joao Vicente <joao@upsites.digital>
2021-04-04 11:58:49 -0300

    Resolução do exercício 111 e 112 onde aprendamos a criar pacotes e o começo de tratamento de erros

---
+++ exercicios/ex0111/teste.py
@@ -0,0 +1,3 @@
+from exercicios.ex0111.utilidadescev import moeda
+preco = float(input('Digite o preço: R$ '))
+moeda.resumo(preco, 20, 12)
---
+++ exercicios/ex0112/teste.py
@@ -0,0 +1,4 @@
+from exercicios.ex0112.utilidadescev import moeda
+from exercicios.ex0112.utilidadescev import dado
+preco = dado.leiaDinheiro('Digite o preço: R$ ')
+moeda.resumo(preco, 20, 12)
