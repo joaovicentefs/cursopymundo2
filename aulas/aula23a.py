8b8cbb641bedafee3337f91ec4080a72cf3abbbe
Joao Vicente <joao@upsites.digital>
2021-04-08 23:56:12 -0300

    Aprendendo a como detalhar o erro customizando a mensagem utilizando uma função especial

--- aulas/aula23a.py
+++ aulas/aula23a.py
@@ -2,8 +2,8 @@ try:
     a = int(input('Numerador: '))
     b = int(input('Denominador: '))
     r = a / b
-except: # Aparece quando "erro" acontece
-    print('Infelizmente tivemos um problema')
+except Exception as erro: # Aparece quando "erro" acontece
+    print(f'ERRO. O problema encontrado foi {erro.__class__}')
 else: # Aparece caso o não apresente nenhum "erro"
     print(f'O resultado é {r:.1f}')
 finally: # Aparece com ou sem o erro.
