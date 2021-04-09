9df636d9e85dfaea19433e789e5f7c0e246b45cf
Joao Vicente <joao@upsites.digital>
2021-04-09 00:38:04 -0300

    Implementando o detalhamento do tratamento de execção baseado no tipo  de exceção apresentado

--- aulas/aula23a.py
+++ aulas/aula23a.py
@@ -2,8 +2,12 @@ try:
     a = int(input('Numerador: '))
     b = int(input('Denominador: '))
     r = a / b
-except Exception as erro: # Aparece quando "erro" acontece
-    print(f'ERRO. O problema encontrado foi {erro.__class__}')
+except (ValueError, TypeError):
+    print('Tivemos um problema com os tipos de dados que você digitou')
+except ZeroDivisionError:
+    print('Não é possível dividir um número por zero!')
+except KeyboardInterrupt:
+    print('Execução interrompida pelo usuário ou o usuário preferiu não informar os dados')
 else: # Aparece caso o não apresente nenhum "erro"
     print(f'O resultado é {r:.1f}')
 finally: # Aparece com ou sem o erro.
